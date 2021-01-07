b3e16276a12b3cfabc454cbccb69559c4ed6ab28
Tanuj Aswani <tanuj.aswani@gmail.com>
2021-01-07 12:52:44 -0700

    removed needless package

--- ps5_notifier.py
+++ ps5_notifier.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from bs4 import BeautifulSoup
-from requests_html import HTMLSession
 import time
 
 username = '' #Costco username
